85a8e47c4f8f8a9ce9cd132d64ab9a12b081f160
Tim Paul <tim.paul@digital.cabinet-office.gov.uk>
2016-01-20 10:43:02 +0000

    Add Baymard country selector demo

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+
 router.get('/', function (req, res) {
   res.render('index');
 });
@@ -12,5 +14,58 @@ router.get('/examples/template-data', function (req, res) {
 });
 
 // add your routes here
+
+var renderCountrySelectorPage = function(params, callback){
+
+  // Load register from JSON file
+
+  var data = {};
+  data.countries = JSON.parse(fs.readFileSync('app/views/patterns/country-selector/data/country-records.json', 'utf8'));
+
+
+  // Load additional country data from JSON file
+
+  var extension = {};
+  extension = JSON.parse(fs.readFileSync('app/views/patterns/country-selector/data/country-records-extension.json', 'utf8'));
+
+
+  // Merge the additional data into the register data
+
+  for(var i = 0; i < data.countries.length; i++){
+
+    for(var j = 0; j < extension.length; j++){
+
+      if (data.countries[i].entry.country == extension[j].country){
+
+          data.countries[i].entry.aliases = extension[j].aliases;  
+          data.countries[i].entry.weighting = extension[j].weighting;
+
+      }
+
+    } 
+
+  }
+
+  // Sort the register alphabetically by country name
+
+  data.countries.sort(function(a, b) {
+    a = a.entry.name.toLowerCase();
+    b = b.entry.name.toLowerCase();
+
+    return a < b ? -1 : a > b ? 1 : 0;
+  })
+
+  return callback(null, data);
+
+}
+
+router.get("/patterns/country-selector/01/", function (req, res, next) {
+  var params = {};
+
+  renderCountrySelectorPage(params, function(error, data){
+    return res.render('patterns/country-selector/01/index', data);
+  })
+
+});
 
 module.exports = router;
