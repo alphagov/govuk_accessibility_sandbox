63e9ab696ecdc7b9792c8e602ffab21d32c28916
Gemma Leigh <hello@gemmaleigh.com>
2016-02-04 18:17:33 +0000

    Move finding and starting GOV.UK modules
    This way, this pattern doesn’t require modifying application.js.
    
    Add a generic start-modules file, which will find and start any GOV.UK
    modules.

---
+++ app/assets/javascripts/govuk/start-modules.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+  // Find and start GOV.UK modules
+  // https://github.com/alphagov/govuk_frontend_toolkit/blob/master/docs/javascript.md#modules
+  GOVUK.modules.start();
+
+});
