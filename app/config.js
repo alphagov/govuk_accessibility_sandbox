c4fa7f5124e03c038164e38b101d35180986830d
Alistair Duggin <alistair.duggin@btinternet.com>
2016-02-04 07:42:43 +0000

    Update name of service

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Accessibility Sandbox",
+  serviceName: "Accessibility sandbox",
 
   // Default port that prototype runs on
   port: '3000',
