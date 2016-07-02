4c3963d1a660e60a4dfcbd21162e040b0ba36467
Mehmet Duran <mehmet.duran@gmail.com>
2016-07-02 12:52:51 +0100

    Typeahead-aria testcase ready

--- app/assets/javascripts/patterns/autocomplete/03/init-autocomplete.js
+++ app/assets/javascripts/patterns/autocomplete/03/init-autocomplete.js
@@ -1,17 +1,63 @@
-$(document).ready(function() {
+$('.typeahead').each(function applyTypeahead() {
+  var $el = $(this);
+  var $parent = $el.parent();
+  $parent.addClass('hidden-hint');
+  var attributes = $el.prop('attributes');
+  var $input = $('<input/>');
+  var selectedValue = $el.val();
+  var typeaheadList = $el.find('option').map(function mapOptions() {
+    if (this.value === '') {
+      // remove any empty values from typeahead
+      /*eslint consistent-return: 0*/
+      return;
+      /*eslint consistent-return: 1*/
+    }
+    return this.value;
+  }).get();
 
-    $('.country-autocomplete').each(function (idx, elm) {
-        GOVUK.autocompletes.add($(elm));
-    });
+  // remove the selectbox
+  $el.remove();
 
-    // Custom events
+  $.each(attributes, function applyAttributes() {
+    $input.attr(this.name, this.value);
+  });
 
-    // Bind all autocomplete events
-    $.each(['initialized', 'opened', 'closed', 'movedto', 'updated'], function (idx, evt) {
-        $(document).bind('typeahead:' + evt, function () {
-            var autocompleteEvent = GOVUK.autocompletes.createEvent(evt);
+  $input.removeClass('js-hidden');
+  $input.addClass('form-control');
+  $input.val(selectedValue);
 
-            autocompleteEvent.trigger.apply(GOVUK.autocompletes, arguments);
-        });
-    });
+  $parent.append($input);
+
+  $input.typeahead({
+    hint: false
+  }, {
+    source: new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: typeaheadList,
+      sorter: function sorter(a, b) {
+        var input = $input.val();
+        var startsWithInput = function startsWithInput(x) {
+          return (x.toLowerCase().substr(0, input.length) === input.toLowerCase()) ? -1 : 1;
+        };
+
+        var compareAlpha = function compareAlpha(x, y) {
+          var less = (x < y) ? -1 : 1;
+          return (x === y) ? 0 : less;
+        };
+
+        var compareStartsWithInput = function compareStartsWithInput(x, y) {
+          var startsWithFirst = startsWithInput(x);
+          var startsWithSecond = startsWithInput(y);
+
+          return (startsWithFirst === startsWithSecond) ? 0 : startsWithFirst;
+        };
+
+        var first = compareStartsWithInput(a, b);
+
+        return (first === 0) ? compareAlpha(a, b) : first;
+      }
+    }),
+    limit: 100
+  });
 });
